419fa54fd7307c6646e34d0d2f0895a474febff1
HuzeyfeG <71647146+HuzeyfeG@users.noreply.github.com>
2024-10-07 16:23:18 +0300

    Add files via upload

--- BrushlessMotorESCDriver/BrushlessMotorESCDriver.ino
+++ BrushlessMotorESCDriver/BrushlessMotorESCDriver.ino
@@ -4,13 +4,11 @@
 Servo esc;
 
 void setup() {
-  // put your setup code here, to run once:
   esc.attach(9, 1000, 2000);
   // Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   esc.write(map(analogRead(pot), 0, 1023, 0, 180));
   // Serial.println(map(analogRead(pot), 0, 1023, 0, 180));
 }
